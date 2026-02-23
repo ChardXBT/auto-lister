33197af37deed12af8aa34a7556225d5047790dc
ChardXBT <ericsuper8890@gmail.com>
2026-02-23 13:38:06 -0500

    update run.sh with auto push

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 cd /root/csfloat-bot
-git pull origin main
+
+git pull origin master:main
+
 python3 bot.py
+
+git add config.json
+git diff --cached --quiet || git commit -m "auto: remove sold items" && git push origin master:main
